05adc3b623127d06f92fdd11ec67d05888dc89a7
Guestding <kiel.dejesus@students.mq.edu.au>
2026-08-25 20:24:24 +1000

    Add basic window
    Added a basic window for us to work from

--- SIm City Project/app.java
+++ SIm City Project/app.java
@@ -1,6 +1,13 @@
+
+import java.awt.Frame;
+
 public class app{
     public static void main(String[] args) {
-        System.out.println("Hello everyone!");
+        System.out.println("Initializing program");
+
+        Frame frame = new Frame("Sim City");
+        frame.setSize(400, 200);
+        frame.setVisible(true);
     }
 
 }
