db616dd1ef3492b3a2d640c029d7aafcaee3546d
Guestding <kiel.dejesus@students.mq.edu.au>
2026-08-26 15:52:51 +1000

    Modify frame size and renamed some files

--- SIm City Project/app.java
+++ SIm City Project/app.java
@@ -1,5 +1,5 @@
 
-import java.awt.Frame;
+import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -7,10 +7,13 @@ public class app{
     public static void main(String[] args) {
         System.out.println("Initializing program");
 
+        //Initialising the Main Window
         Frame frame = new Frame("Sim City");
-        frame.setSize(400, 200);
+        frame.setSize(800, 600);
         frame.setVisible(true);
+        frame.setBackground(Color.green);
 
+        //Method to allow window closing
         frame.addWindowListener(new WindowAdapter(){
             public void windowClosing(WindowEvent we){
                 System.exit(0);
