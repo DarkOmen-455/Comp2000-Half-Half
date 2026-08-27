ab7c2f3cb1847e7428884d9e41343c9384c968a3
Klein-Morettiii <bryn.lee.sydeny@gmail.com>
2026-08-27 13:07:11 +1000

    added classes and changed some stuff with frames

---
+++ SIm City Project/Building.java
@@ -0,0 +1,23 @@
+abstract class Building {
+    private int x, y;
+
+    public void setX(int X){
+        this.x = X;
+    }
+
+    public void setY(int Y){
+        this.y = Y;
+    }
+
+    public int getX(){
+        return x;
+    } 
+
+    public int getY(){
+        return y;
+    }
+
+    public abstract void build(int x, int y);
+    
+    public abstract void destroy(int x, int y);
+}
