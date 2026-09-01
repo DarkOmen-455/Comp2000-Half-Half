a99956a2fc4b4a1666ac7c528d122366a90b62f8
Klein-Morettiii <bryn.lee.sydeny@gmail.com>
2026-09-01 12:35:49 +1000

    added placement functionality for different drawable objects

--- Sim City Project/Main.java
+++ Sim City Project/Main.java
@@ -1,10 +1,13 @@
+import java.awt.Graphics2D;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Initializing program");
-        Window w = new Window(800, 600);//width then height
+        Window w = new Window(800, 600); // width then height
 
-        //to keep track of placemetn of buildings 
-        Building[][] arr = new Building[14][10];
+        House test = new House(3, 3);
+        w.place(5, 5, test);
 
+        Apartment test2 = new Apartment();
+        w.place (6,6,test2);
     }
 }
--- Sim City Project/Window.java
+++ Sim City Project/Window.java
@@ -3,23 +3,25 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 public class Window extends Frame {
 
-    
-    int width; 
-    int height;
+    private int width;
+    private int height;
+    private Building[][] grid;
+
     // constructor, acts as a decorator for our main window
     Window(int width, int height){
+        this.width = width;
+        this.height = height;
+        this.grid = new Building[10][14];
         this.setVisible(true);
         this.setSize(width,height);
         this.setTitle("Sim City");
         this.setBackground(Color.green);
 
-   this.addWindowListener(new WindowAdapter(){
+        this.addWindowListener(new WindowAdapter(){
             public void windowClosing(WindowEvent we){
                 System.exit(0);
             }
-
         });
-
     }
  
 
@@ -48,6 +50,14 @@ public class Window extends Frame {
         drawRedOval(ovalg2d,2,2);
         ///removeDrawing(ovalg2d, 2, 2);
 
+        //testing 
+        for (int x = 0; x < grid.length; x++) {
+            for (int y = 0; y < grid[x].length; y++) {
+                if (grid[x][y] != null) {
+                    grid[x][y].build(g2d, x, y);
+                }
+            }
+        }
     }
     
     public void drawRedOval(Graphics2D ovalg2d, int x, int y){;// draws red oval
@@ -56,11 +66,15 @@ public class Window extends Frame {
         ovalg2d.fillOval((x*50)+7,(y*50)+7,35,35);
     }
     
-    public void removeDrawing( int x, int y){// replaces current object with green reactacngle 
-        Graphics2D ob = (Graphics2D) g;
-        ob.setColor(Color.green);
-        ob.fillRect((x*50)+1,(y*50)+1, 48,48);
+    public void removeDrawing(Graphics2D g2d, int x, int y){ // replaces current object with green rectangle
+        g2d.setColor(Color.green);
+        g2d.fillRect((x*50)+1,(y*50)+1, 48,48);
     }
-
+    public void place(int x, int y, Building b){
+        if (x >= 0 && x < grid.length && y >= 0 && y < grid[x].length) {
+            grid[x][y] = b;
+            repaint();
+        }
+    }
     
 }
