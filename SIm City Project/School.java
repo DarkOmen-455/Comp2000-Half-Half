6e4f5b22f4aa4648b5de0738a2ce470d2949d057
Klein-Morettiii <bryn.lee.sydeny@gmail.com>
2026-09-02 13:05:06 +1000

    added count functionality

--- SIm City Project/School.java
+++ SIm City Project/School.java
@@ -1,6 +1,7 @@
 public class School extends Building {
     private int children; 
     
+    
     //rule (can be changed later): if 200 children in 5 square radious ad school.
     public void build(int x, int y){
 
