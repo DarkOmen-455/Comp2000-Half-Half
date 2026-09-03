54f807b09f95cba014a26d4fe341e41b1fdfffd4
vikramsingh2-jpg <vikram.singh2@students.mq.edu.au>
2026-09-03 20:07:45 +1000

    Update Shop.java
    added shop code

--- SIm City Project/Shop.java
+++ SIm City Project/Shop.java
@@ -1,3 +1,24 @@
+import java.awt.Graphics2D;
+import java.awt.Color;
+
 public class Shop extends Building{
     
-}
+    // stop the shops from being added to the cities population 
+    @Override
+    public int getPopulation() {
+        return 0;
+     }
+
+     //how many shops the city should have 
+     public static int shopsRequired(int population) { 
+        return population / 50; 
+     }
+
+     // draws a blue square to represent a shop
+    @Override
+    public void build(Graphics2D g2d, int x, int y) {
+        g2d.setColor(Color.Blue);
+        g2d.fillRect((x * 50) + 7,(y * 50) +7, 35, 35);
+    }
+    } 
+
