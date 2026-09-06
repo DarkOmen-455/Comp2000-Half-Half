5bb5bf0ff4d7fca7713201bd794b58bfee771886
Klein-Morettiii <bryn.lee.sydney@gmail.com>
2026-09-06 22:18:37 +1000

    create placement logic

--- Sim City Project/Window.java
+++ Sim City Project/Window.java
@@ -58,23 +58,39 @@ public class Window extends Frame {
         });
     }
 
-    public boolean toBuildSchool(){
-        boolean total = false;
-        int children = 0;
-        for (int i = 0; i<grid.length; i++){
-            for (int z = 0; z<grid[i].length; z++){
-                if (grid[z][i] instanceof House){
-                    total += grid[z][i].getChildren();
-                    continue;
-                }
-                if (grid[z][z] instanceof Apartment){
-                    total += grid[z][i].get();
-                    continue;
+    public int getBuilding(String temp){
+        //pass in building subclass, returns number of subclasses in grid
+        int total = 0;
+        for (int x = 0; x < grid.length; x++) {
+            for (int y = 0; y < grid[x].length; y++) {
+                if (grid[x][y] == null)
+                    continue; 
+                if (grid[x][y].getName() == temp){
+                    total++;
                 }
             }
         }
         return total;
     }
+
+
+    // public boolean toBuildSchool(){
+    //     boolean total = false;
+    //     int children = 0;
+    //     for (int i = 0; i<grid.length; i++){
+    //         for (int z = 0; z<grid[i].length; z++){
+    //             if (grid[z][i] instanceof House){
+    //                 total += grid[z][i].getChildren();
+    //                 continue;
+    //             }
+    //             if (grid[z][z] instanceof Apartment){
+    //                 total += grid[z][i].get();
+    //                 continue;
+    //             }
+    //         }
+    //     }
+    //     return total;
+    // }
  
 
     // paint method used to add graphics to the screen
@@ -121,12 +137,77 @@ public class Window extends Frame {
 
     public void place(int x, int y, Building b){
 
-        System.out.println(grid.length);
+        //System.out.println(grid.length);
         if (x >= 0 && x < grid.length && y >= 0 && y < grid[x].length) {
             grid[x][y] = b;
             repaint();
         }
     }
 
-    
+    public int[] findPlaceLocation(){
+        // find square 
+        int startX = height/50;
+        int startY = width/50;
+
+        //random 1-4, gives us sector of grid, havent impletmented
+        int ram = 1; 
+
+        //
+        int x = startX -1; 
+        int y = startY -1; 
+        int[] total = new int[1];
+        boolean even = false;  // so i dont have to repreaste code 
+        while (startX < width/50 || startY < height/50){
+            //check if in bottom left or right will implerment for top as well later
+            // to move x and y to next diagional square 
+            /*
+            0 0 0 0 0        c = centerx and center y
+            0 0 0 0 0
+            - - c 0 0
+            0 c | 0 0
+            c 0 | 0 0
+            */
+            if (ram/2 == 0){
+                startX++;
+                startY--;
+                even = true;
+            }
+            else {
+                startX--;
+                startY++;
+            }
+            x = startX; y = startY;// to reset x and y
+            while(x<=(width/50)/2){// while x is not toching invissable center line 
+                if (spare(startX,startY)){
+                    total[0] = x;
+                    total[1] = y;
+                    return total;
+                }
+                if (even)
+                    x--;
+                else{
+                    x++;
+                }
+            }
+            while(y<=(height/50)/2){// same but for y
+                if (spare(startX,startY)){
+                    total[0] = x;
+                    total[1] = y;
+                    return total;
+                }
+                if(even)
+                    y++;
+                else{
+                    y--;
+                }
+            }
+        }
+        return null;
+    }
+    public boolean spare(int x, int y){
+        if (grid[x][y] == null){
+            return true;
+        }
+        return false;
+    }
 }
